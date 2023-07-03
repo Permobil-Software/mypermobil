3771d1583b0aa1943e84b424a03a850c8419dcbb
Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>
2023-07-03 14:20:40 +0200

    bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mypermobil",
-    version="0.1.2",
+    version="0.1.3",
     description="A Python wrapper for the MyPermobil API",
     url="https://github.com/IsakNyberg/MyPermobil-API",
     author="Isak Nyberg",
